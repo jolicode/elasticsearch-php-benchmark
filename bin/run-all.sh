7984f5153ed84f51a631fa3d53f3067ea7453769
Zachary Tong <zacharyjtong@gmail.com>
2013-11-21 14:26:46 -0500

    Clear index/cache before each test
    The index and caches need to be deleted + reloaded before each
    test run.  Results are not comparable unless each client is
    operating on the same clean slate.
    
    If you don't clear the test results, ES will be "warmed" by
    queries from the previous client, leading to reduced latency

--- bin/run-all.sh
+++ bin/run-all.sh
@@ -1,14 +1,15 @@
 ./bin/load-data.sh
-
-curl -XPOST 'http://localhost:9200/client_bench/_refresh'
 sleep 5
 ./console run elasticsearch --hide-errors=1
-curl -XPOST 'http://localhost:9200/client_bench/_refresh'
+
+./bin/load-data.sh
 sleep 5
 ./console run sherlock --hide-errors=1
-curl -XPOST 'http://localhost:9200/client_bench/_refresh'
+
+./bin/load-data.sh
 sleep 5
 ./console run elastica --hide-errors=1
-curl -XPOST 'http://localhost:9200/client_bench/_refresh'
+
+./bin/load-data.sh
 sleep 5
 ./console run nervetattoo --hide-errors=1
