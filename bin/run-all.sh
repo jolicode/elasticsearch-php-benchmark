524b471b58e4cc67e6e5ae9f63f05d1b94ba7126
Zachary Tong <zacharyjtong@gmail.com>
2013-11-21 15:41:03 -0500

    Warm the JVM
    This loops over the tests and executes them against elasticsearch
    so that we can warm the JVM and its JIT output.
    
    Also pipes output to null for sanity

--- bin/run-all.sh
+++ bin/run-all.sh
@@ -1,15 +1,42 @@
-./bin/load-data.sh
+
+printf "Starting Elasticsearch...\n"
+$1
+sleep 15
+
+printf "Warming the JVM...\n"
+for i in {1..5}
+do
+    ./bin/load-data.sh > /dev/null 2>&1
+    ./console run elasticsearch --hide-errors=1 > /dev/null 2>&1
+    printf "."
+
+    ./bin/load-data.sh > /dev/null 2>&1
+    ./console run sherlock --hide-errors=1 > /dev/null 2>&1
+    printf "."
+
+    ./bin/load-data.sh > /dev/null 2>&1
+    ./console run elastica --hide-errors=1 > /dev/null 2>&1
+    printf "."
+
+    ./bin/load-data.sh > /dev/null 2>&1
+    ./console run nervetattoo --hide-errors=1 > /dev/null 2>&1
+    printf "."
+done
+
+print "\nBegin testing...\n"
+
+./bin/load-data.sh > /dev/null 2>&1
 sleep 5
 ./console run elasticsearch --hide-errors=1
 
-./bin/load-data.sh
+./bin/load-data.sh > /dev/null 2>&1
 sleep 5
 ./console run sherlock --hide-errors=1
 
-./bin/load-data.sh
+./bin/load-data.sh > /dev/null 2>&1
 sleep 5
 ./console run elastica --hide-errors=1
 
-./bin/load-data.sh
+./bin/load-data.sh > /dev/null 2>&1
 sleep 5
 ./console run nervetattoo --hide-errors=1
