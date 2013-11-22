000df28f85295bc85a06e5b2891a8f1f9144d6d6
Zachary Tong <zacharyjtong@gmail.com>
2013-11-22 08:08:36 -0500

    Specify benchmark type for JVM-warming runs

--- bin/run-all.sh
+++ bin/run-all.sh
@@ -7,23 +7,23 @@ printf "Warming the JVM...\n"
 for i in {1..5}
 do
     ./bin/load-data.sh > /dev/null 2>&1
-    ./console run elasticsearch --hide-errors=1 > /dev/null 2>&1
+    ./console run elasticsearch persistent --hide-errors=1 > /dev/null 2>&1
     printf "."
 
     ./bin/load-data.sh > /dev/null 2>&1
-    ./console run sherlock --hide-errors=1 > /dev/null 2>&1
+    ./console run sherlock persistent --hide-errors=1 > /dev/null 2>&1
     printf "."
 
     ./bin/load-data.sh > /dev/null 2>&1
-    ./console run elastica --hide-errors=1 > /dev/null 2>&1
+    ./console run elastica persistent --hide-errors=1 > /dev/null 2>&1
     printf "."
 
     ./bin/load-data.sh > /dev/null 2>&1
-    ./console run nervetattoo --hide-errors=1 > /dev/null 2>&1
+    ./console run nervetattoo persistent --hide-errors=1 > /dev/null 2>&1
     printf "."
 done
 
-printf "\n------------------------------ Transient Tests ------------------------------\n"
+printf "done!\n\n------------------------------ Transient Tests ------------------------------\n"
 
 ./bin/load-data.sh > /dev/null 2>&1
 sleep 5
